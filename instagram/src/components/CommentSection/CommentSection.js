2790e49f3fdd76293fe68c284b56c7381ca71690
JamesB <jblazer1@yahoo.com>
2019-03-20 15:49:03 -0700

    addNewComment NOT working

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,17 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Comment from "./Comment";
+import NewComment from "./NewComment";
 
-const CommentSection = props => {
-  // console.log(props.comment);
-  return (
-    <div className="comment-container">
-      {props.comments.map(comment => (
-        <Comment key={comment.id} comment={comment} />
-      ))}
-    </div>
-  );
-};
+class CommentSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      comments: [],
+      newComment: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ comments: this.props.comments });
+  }
+
+  newCommentHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addNewComment = (event, index) => {
+    event.preventDefault();
+    if (this.state.comments === "") {
+      alert("You have to enter a  comment you igit!");
+    } else {
+      this.setState({
+        comments: [
+          ...this.state.comments,
+          {
+            username: this.state.username,
+            text: this.state.text
+          }
+        ],
+        newComment: ""
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="comment-container">
+        {this.state.comments.map(comment => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+        <NewComment
+          addNewComment={this.addNewComment}
+          newCommentHandler={this.newCommentHandler}
+        />
+      </div>
+    );
+  }
+}
 
 export default CommentSection;
 
